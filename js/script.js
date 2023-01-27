ea7d570aa69fd0dd91921567e0aa5710ac61221d
Angelika Krygier <angelika.krygier96@gmail.com>
2023-01-27 10:45:01 +0100

    Tidied up of JS code

--- js/script.js
+++ js/script.js
@@ -1,35 +1,35 @@
-let formElement = document.querySelector(".js-form");
-let amountElement = document.querySelector(".js-amount");
-let resultElement = document.querySelector(".js-result");
-let currencyRateElement = document.querySelector(".js-currency");
+{
+    const formElement = document.querySelector(".js-form");
+    const amountElement = document.querySelector(".js-amount");
+    const resultElement = document.querySelector(".js-result");
+    const currencyRateElement = document.querySelector(".js-currency");
+    const currency = currencyRateElement.value;
 
 
-let currencyEUR = 4.6946;
-let currencyUSD = 4.3398;
-let currencyCHF = 4.7005;
+    const calculateResult = (currency) => {
 
+        const currencyEUR = 4.6946;
+        const currencyUSD = 4.3398;
+        const currencyCHF = 4.7005;
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    let amount = amountElement.value;
-    let currency = currencyRateElement.value;
-
-    let rate;
-
-    switch (currency) {
-        case "EUR":
-            rate = currencyEUR;
-            break;
-        case "USD":
-            rate = currencyUSD;
-            break;
-        case "CHF":
-            rate = currencyCHF;
+        switch (currency) {
+            case "EUR":
+                return currencyEUR;
+            case "USD":
+                return currencyUSD;
+            case "CHF":
+                return currencyCHF;
+        }
     }
 
-    const result = amount * rate;
+    formElement.addEventListener("submit", (event) => {
+        event.preventDefault();
 
-    resultElement.innerText = result.toFixed(2);
+        const amount = amountElement.value;
+        const rate = calculateResult(currency)
+        const result = amount * rate;
 
-});
+        resultElement.innerText = result.toFixed(2);
+
+    });
+}
